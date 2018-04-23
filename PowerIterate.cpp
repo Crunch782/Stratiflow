d078fcabc388d2dce0bf929f025f4ecba937137a
Jeremy Parker <jpp39@cam.ac.uk>
2018-04-23 10:12:29 +0100

    Better power iteration

--- PowerIterate.cpp
+++ PowerIterate.cpp
@@ -4,38 +4,61 @@ int main(int argc, char* argv[])
 {
     const stratifloat T = 10;
 
-    Ri = std::stof(argv[2]);
+    if (argc == 2)
+    {
+        Ri = std::stof(argv[1]);
+    }
+    else
+    {
+        Ri = std::stof(argv[2]);
+    }
 
     StateVector stationaryPoint;
-    stationaryPoint.LoadFromFile(argv[1]);
+
+    if (argc == 3)
+    {
+        stationaryPoint.LoadFromFile(argv[1]);
+    }
 
     StateVector stationaryPointEnd;
 
     stationaryPoint.FullEvolve(T, stationaryPointEnd, false, false);
 
-    StateVector b_k;
-    b_k.Randomise(0.0001, true);
-    StateVector b_k1;
+    // separate real and imaginary parts
+    StateVector b_kr;
+    b_kr.Randomise(0.0001, true);
+    StateVector b_ki;
+    b_ki.Randomise(0.0001, true);
+
+    StateVector b_kr1;
+    StateVector b_ki1;
 
     int iterations = 0;
     while (true)
     {
         // normalise
-        b_k *= 1/(b_k.Norm());
+        stratifloat norm = b_kr.Norm() + b_ki.Norm();
+        b_kr *= 1/norm;
+        b_ki *= 1/norm;
 
         // power iterate
-        b_k.LinearEvolve(T, stationaryPoint, stationaryPointEnd, b_k1);
+        b_kr.LinearEvolve(T, stationaryPoint, stationaryPointEnd, b_kr1);
+        b_ki.LinearEvolve(T, stationaryPoint, stationaryPointEnd, b_ki1);
 
         // largest eigenvalue (in magnitude) of exponential matrix
-        stratifloat mu = b_k1.Dot(b_k);
+        complex mu = (b_kr.Dot(b_kr1) + b_ki.Dot(b_ki1));
+                   + i*(b_kr.Dot(b_ki1) - b_ki.Dot(b_kr1));
 
-        stratifloat eigenvalueGuess = log(mu)/T;
+        // abs and arg of exp(A) eigenvalue give real/imag of eigenvalue of A
+        stratifloat growthGuess = log(abs(mu))/T;
+        stratifloat phaseSpeed = arg(mu) / T; // only works if T sufficiently small
 
         iterations++;
-        std::cout << "ITERATION " << iterations << ", growth rate: " << eigenvalueGuess << std::endl;
+        std::cout << "ITERATION " << iterations << ", growth rate: " << growthGuess << std::endl;
+        std::cout << "phase speed: " << phaseSpeed << std::endl;
 
         // for next step
-        b_k = b_k1;
-
+        b_kr = b_kr1;
+        b_ki = b_ki1;
     }
 }
